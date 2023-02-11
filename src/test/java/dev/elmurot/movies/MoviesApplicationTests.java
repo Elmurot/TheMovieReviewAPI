76bbf7708a61a4869604716f73b53d78e9ad5c59
Elmurot <elmurotyangiboev@gmail.com>
2023-02-11 12:53:01 -0500

    Initial commit

---
+++ src/test/java/dev/elmurot/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elmurot.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
